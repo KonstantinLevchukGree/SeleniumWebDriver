f1659155f04c0bf0f207f8e41973ddb0d3c0227c
KonstantinLevchuk <KonstantinLevchuk@coherentsolutions.com>
2023-01-10 14:18:52 +0100

    added TestNG tests

---
+++ src/test/java/testNgTests/RealItemTest.java
@@ -0,0 +1,24 @@
+package testNgTests;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import shop.RealItem;
+
+public class RealItemTest {
+    private RealItem car;
+
+    @Parameters({"weight car"})
+    @BeforeClass(groups = {"group test"})
+    public void initRealItem(String weightCar) {
+        car = new RealItem();
+        car.setWeight(Double.parseDouble(weightCar));
+    }
+
+    @Parameters({"weight car"})
+    @Test(groups = {"group test"})
+    public void checkWeightCarTest(String weightCar) {
+        Assert.assertEquals(car.getWeight(), Double.parseDouble(weightCar), "Weight Car not equal Weight Car in test data");
+    }
+}
